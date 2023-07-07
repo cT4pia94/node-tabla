0355242be4de78f20a2e1a7db4f0a22c01556893
Carlos Tapia <carlostapie94@gmail.com>
2023-07-07 16:11:09 -0400

    Primer commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('./config/yargs');
+
+
+console.clear();
+
+// const [ , , arg3 = 'base=5'] = process.argv;
+// const [ , base = 5 ] = arg3.split('=');
+
+// console.log(process.argv);
+// console.log(argv);
+
+// console.log('base: yargs', argv.b);
+
+
+
+// const base = 7;
+
+crearArchivo(argv.b, argv.l, argv.h)
+    .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
+    .catch(err => console.log(err));
+
+
+
+
---
+++ config/yargs.js
@@ -0,0 +1,32 @@
+const argv = require('yargs')
+                .options({
+                    'b': {
+                        alias: 'base',
+                        type: 'number',
+                        demandOption: true,
+                        describe: 'Es la base de la tabla de multiplicar'
+                    },
+                    'l': {
+                        alias: 'listar',
+                        type: 'boolean',
+                        demandOption: false,
+                        default: false,
+                        describe: 'Muestra la tabla en consola'
+                    },
+                    'h': {
+                        alias: 'hasta',
+                        type: 'number',
+                        demandOption: false,
+                        default: 10,
+                        describe: 'Cantidad de veces que se multiplica la base'
+                    }
+                })
+                .check( ( argv, options ) => {
+                    if( isNaN( argv.b ) ){
+                        throw 'La base tiene que ser un número';
+                    }
+                    return true;
+                })
+                .argv;
+
+module.exports = argv;
